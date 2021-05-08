b56a2b9395bd3caef1b8e8065f29edd281b8bfb6
Bianca <biancancanal@gmail.com>
2021-05-07 19:00:11 -0500

    Link NewQuote and NewButton

--- src/containers/NewQuote.js
+++ src/containers/NewQuote.js
@@ -19,6 +19,15 @@ export default class NewQuote extends Component {
     render() {
         return (
             <div>
+                <NewButton/>
+                <form onSubmit={this.handleSubmit}>
+                    <label>ID:</label><br/>
+                    <input name="id" value={this.state} onChange={this.handleChange} type="text"/><br/>
+                    <label>ID:</label><br/>
+                    <input name="quote" value={this.state} onChange={this.handleChange} type="text"/><br/>
+                    <label>ID:</label><br/>
+                    <input name="author" value={this.state} onChange={this.handleChange} type="text"/><br/>
+                </form>
                 <h3> I am a form </h3>
             </div>
         )
