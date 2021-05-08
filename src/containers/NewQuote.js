64c6aa9d9405f22e99e216125a249e1f0cb38428
Bianca <biancancanal@gmail.com>
2021-05-08 18:24:58 -0500

    Setup handleChange

--- src/containers/NewQuote.js
+++ src/containers/NewQuote.js
@@ -3,32 +3,36 @@ import NewButton from '../components/NewButton'
 
 export default class NewQuote extends Component {
     state = {
-        id: "",
+        keyword: "", 
         quote: "",
         author: ""
     }
 
     handleChange = (e) => {
-
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
+
     }
     
     render() {
         return (
             <div>
-                <NewButton/>
                 <form onSubmit={this.handleSubmit}>
-                    <label>ID:</label><br/>
-                    <input name="id" value={this.state} onChange={this.handleChange} type="text"/><br/>
-                    <label>ID:</label><br/>
-                    <input name="quote" value={this.state} onChange={this.handleChange} type="text"/><br/>
-                    <label>ID:</label><br/>
-                    <input name="author" value={this.state} onChange={this.handleChange} type="text"/><br/>
+                    <h1>Add A New Quote!</h1>
+                    <label>Key Words:</label><br/>
+                    <input name="keyword" value={this.state.keyword} onChange={this.handleChange} type="text"/><br/>
+                    <label>Quote:</label><br/>
+                    <textarea name="quote" value={this.state.quote} onChange={this.handleChange} /><br/>
+                    <label>Author:</label><br/>
+                    <input name="author" value={this.state.author} onChange={this.handleChange} type="text"/><br/>
+                    <br></br>                    
+                    <input type="submit"/>
                 </form>
-                <h3> I am a form </h3>
             </div>
         )
     }
