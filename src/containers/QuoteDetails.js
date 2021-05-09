59a3aa72c32d516c04d7f39d011df96639175b75
Bianca <biancancanal@gmail.com>
2021-05-08 19:20:42 -0500

    QuoteDetails Fetch

--- src/containers/QuoteDetails.js
+++ src/containers/QuoteDetails.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 class QuoteDetails extends Component {
+    state = {
+        quotes: []
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:3001/quotes${this.props.match.params.id}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            this.setState({
+                quotes: data
+            })
+        })
+    }
     render() {
+        // const quotes = this.state.quotes.map((q) => {q.id})
         return (
             <div>
                 Full Quote
+                {/* {quotes} */}
             </div>
         )
     }
