860d0fde45d2bd0f44771d6725dcee8cb7591df7
Bianca <biancancanal@gmail.com>
2021-05-13 11:14:57 -0500

    Clean Drafted Code

--- src/components/QuoteLink.js
+++ src/components/QuoteLink.js
@@ -1,24 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 const QuoteLink = ({quote}) => {
-    // const deleteQuote = (quote) => {
-    //     console.log(quote)
-    //     fetch(`http://localhost:3001/quotes/${this.props.match.params.id}`, {
-    //         method: 'DELETE',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //     })
-    //     .then(res => res.json())
-    // }
-    
     return (
         <div>
             <li>
             <Link to={`/quotes/${quote.id}`}>
                 {quote.keyword} 
             </Link>
-            {/* <button onChange={deleteQuote}> x </button> */}
             </li>
         </div>
     )
